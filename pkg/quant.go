08a65418fdd9d3f918c8fab16c620dc96cb643b4
zhuyanxi <547110356@qq.com>
2026-06-05 07:52:35 +0800

    Update quant.go

--- pkg/quant.go
+++ pkg/quant.go
@@ -8,6 +8,10 @@ package pkg
 // 2005-01-04,996.28,996.28,982.99,989.98
 // 2005-01-05,989.87,1018.26,988.57,1013.58
 // 2005-01-06,1014.97,1014.97,1001.21,1005.47
-func ATR(n int, path string) {
+func AtrOut(n int, path string) {
 
+}
+
+func atr(n int, closes []float64) float64 {
+	return 0
 }
